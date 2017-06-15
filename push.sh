758ba2b3aa4ec332ff950ca054e0b1149faad22c
czyt1988 <czy.t@163.com>
2017-06-15 09:14:45 +0800

    删除一些无用脚本

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /mnt/hgfs/VMShare/sa
-git push origin dev:dev
-git push origin dev:dev
-git push origin dev:dev
-git push origin dev:dev
-git push origin dev:dev
-git push origin dev:dev
