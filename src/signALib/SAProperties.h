61309c1411d602c45c866515065ebf4cb6b7592e
czyt1988 <czy.t@163.com>
2020-11-17 23:33:15 +0800

    完善属性组内容

--- src/signALib/SAProperties.h
+++ src/signALib/SAProperties.h
@@ -6,11 +6,13 @@
 
 /**
  * @brief 参考Java Properties 类封装的属性类，负责SA的属性传递
+ *
+ *
  */
 class SALIB_EXPORT SAProperties : public QVariantHash
 {
 public:
-    SAProperties();
+    SAProperties() = default;
 
     //获取属性
     QVariant getProperty(const QString& key) const;
@@ -21,5 +23,35 @@ public:
 };
 
 
+/**
+ * @brief 属性组
+ *
+ */
+class SALIB_EXPORT SAPropertiesGroup : public QHash<QString, SAProperties>
+{
+public:
+    SAPropertiesGroup() = default;
+    //获取属性
+    QVariant getProperty(const QString& group, const QString& key) const;
+    QVariant getProperty(const QString& group, const QString& key, const QVariant& defaultProperty) const;
+
+    //获取一组属性，必须先确保有这个分组
+    const SAProperties& constProperties(const QString& group) const;
+
+    //获取一组属性的引用，如果没有，会插入一个默认属性
+    SAProperties& properties(const QString& group);
+
+    //获取一组属性
+    SAProperties getProperties(const QString& group);
+
+    //设置一组属性
+    void setProperties(const QString& group, const SAProperties& propertys);
+
+    //设置属性
+    void setProperty(const QString& group, const QString& key, const QVariant& value);
+
+    //判断是否存在分组
+    bool hasGroup(const QString& group);
+};
 
 #endif // SAPROPERTIES_H
